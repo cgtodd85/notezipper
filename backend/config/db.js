9f4268c1df091c4e7a010f9d64747c5574727c7f
Connor Todd <cgtodd85@gmail.com>
2021-12-20 15:45:27 -0500

    added connection to local db and installed packages

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,14 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost/notezip1",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error: ${error.message}`.red.bold);
